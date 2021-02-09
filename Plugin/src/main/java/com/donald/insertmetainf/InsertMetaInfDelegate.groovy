6dd37ea32ef9ca83cad973e2af8b206852bb2d6e
donald <onsemise@qq.com>
2021-02-09 12:14:23 +0800

    fix bug

--- Plugin/src/main/java/com/donald/insertmetainf/InsertMetaInfDelegate.groovy
+++ Plugin/src/main/java/com/donald/insertmetainf/InsertMetaInfDelegate.groovy
@@ -1,39 +1,20 @@
 package com.donald.insertmetainf
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.LibraryPlugin
-import com.android.build.gradle.TestExtension
-import com.android.build.gradle.TestPlugin
+
 import org.gradle.api.Project
 
 class InsertMetaInfDelegate {
 
     void apply(Project project) {
         project.extensions.create('insertMetaInf', InsertMetaInfExtension)
-        InsertMetaInfTask insertMetaInfTask = project.task("insertMetaInfTask", InsertMetaInfTask)
+        InsertMetaInfTask insertMetaInfTask = project.tasks.create("insertMetaInfTask", InsertMetaInfTask)
 
         def resDir = new File(project.buildDir, 'generated/insertMetaInf')
         def destDir = new File(resDir, 'META-INF')
-
-        boolean isLibrary = project.plugins.hasPlugin(LibraryPlugin)
-        if (isLibrary) {
-            project.extensions.getByType(LibraryExtension).sourceSets({
-                main.resources.srcDirs += resDir
-            })
-        } else if (project.plugins.hasPlugin(TestPlugin)) {
-            project.extensions.getByType(TestExtension).sourceSets({
-                main.resources.srcDirs += resDir
-            })
-        } else if (project.plugins.hasPlugin(AppPlugin)) {
-            project.extensions.getByType(AppExtension).sourceSets({
-                main.resources.srcDirs += resDir
-            })
-        }
-
         insertMetaInfTask.destDir = destDir
+
         project.afterEvaluate {
+            project.extensions.android.sourceSets.main.resources.srcDirs += resDir
             insertMetaInfTask.insertMetaInf = project.extensions.insertMetaInf
             project.tasks.findAll { task ->
                 task.name.startsWith('generate') && task.name.endsWith('Resources')
